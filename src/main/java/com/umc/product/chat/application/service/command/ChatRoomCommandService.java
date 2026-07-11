fa7386b05f56ea635141b5ba33b6ecf969d0019d
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2026-07-11 22:18:05 +0900

    feat: 채팅방별 메시지 고정 및 해제 기능 추가

--- src/main/java/com/umc/product/chat/application/service/command/ChatRoomCommandService.java
+++ src/main/java/com/umc/product/chat/application/service/command/ChatRoomCommandService.java
@@ -7,8 +7,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.umc.product.chat.application.port.in.command.CreateChatRoomUseCase;
 import com.umc.product.chat.application.port.in.command.DeleteChatRoomUseCase;
+import com.umc.product.chat.application.port.in.command.ManageChatRoomPinnedMessageUseCase;
 import com.umc.product.chat.application.port.in.command.dto.CreateChatRoomCommand;
+import com.umc.product.chat.application.port.in.command.dto.PinChatRoomMessageCommand;
 import com.umc.product.chat.application.port.in.query.dto.ChatRoomInfo;
+import com.umc.product.chat.application.port.out.LoadChatMessagePort;
 import com.umc.product.chat.application.port.out.LoadChatRoomPort;
 import com.umc.product.chat.application.port.out.SaveChatMemberPort;
 import com.umc.product.chat.application.port.out.SaveChatRoomPort;
@@ -20,10 +23,14 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class ChatRoomCommandService implements CreateChatRoomUseCase, DeleteChatRoomUseCase {
+public class ChatRoomCommandService implements
+    CreateChatRoomUseCase,
+    DeleteChatRoomUseCase,
+    ManageChatRoomPinnedMessageUseCase {
 
     private final SaveChatRoomPort saveChatRoomPort;
     private final LoadChatRoomPort loadChatRoomPort;
+    private final LoadChatMessagePort loadChatMessagePort;
     private final SaveChatMemberPort saveChatMemberPort;
 
     @Override
@@ -37,5 +44,20 @@ public class ChatRoomCommandService implements CreateChatRoomUseCase, DeleteChat
     public void delete(Long roomId) {
         ChatRoom chatRoom = loadChatRoomPort.getById(roomId);
         saveChatRoomPort.delete(chatRoom);
+    }
+
+    @Override
+    public void pin(PinChatRoomMessageCommand command) {
+        ChatRoom chatRoom = loadChatRoomPort.getById(command.roomId());
+        loadChatMessagePort.getByIdAndRoomId(command.messageId(), command.roomId());
+        chatRoom.pinMessage(command.messageId());
+        saveChatRoomPort.save(chatRoom);
+    }
+
+    @Override
+    public void unpin(Long roomId) {
+        ChatRoom chatRoom = loadChatRoomPort.getById(roomId);
+        chatRoom.unpinMessage();
+        saveChatRoomPort.save(chatRoom);
     }
 }
