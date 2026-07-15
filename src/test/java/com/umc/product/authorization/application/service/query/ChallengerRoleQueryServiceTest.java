UMC-PRODUCT/umc-product-server

refactor: split challenger role usecases

src/test/java/com/umc/product/authorization/application/service/query/ChallengerRoleQueryServiceTest.java
@@ -1,0 +1,96 @@
+package com.umc.product.authorization.application.service.query;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import com.umc.product.authorization.application.port.in.query.CheckChallengerAuthorityUseCase;
+import com.umc.product.authorization.application.port.in.query.ListChallengerRoleUseCase;
+import com.umc.product.authorization.application.port.in.query.dto.ChallengerRoleInfo;
+import com.umc.product.authorization.application.port.out.LoadChallengerRolePort;
+import com.umc.product.authorization.domain.ChallengerRole;
+import com.umc.product.common.domain.enums.ChallengerRoleType;
+import com.umc.product.organization.application.port.in.query.GetGisuUseCase;
+import com.umc.product.organization.application.port.in.query.dto.gisu.GisuInfo;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("ChallengerRoleQueryService")
+class ChallengerRoleQueryServiceTest {
+
+    private static final Long MEMBER_ID = 1L;
+    private static final Long GISU_ID = 9L;
+    private static final Long SCHOOL_ID = 30L;
+
+    @Mock
+    LoadChallengerRolePort loadChallengerRolePort;
+
+    @Mock
+    GetGisuUseCase getGisuUseCase;
+
+    @Test
+    @DisplayName("조회 전용 UseCase로 멤버의 역할 목록을 조회한다")
+    void list_by_member_id() {
+        ChallengerRoleQueryService sut = new ChallengerRoleQueryService(loadChallengerRolePort, getGisuUseCase);
+        ListChallengerRoleUseCase useCase = sut;
+        ChallengerRole role = ChallengerRole.create(
+            10L,
+            ChallengerRoleType.SCHOOL_PRESIDENT,
+            SCHOOL_ID,
+            null,
+            GISU_ID
+        );
+        given(loadChallengerRolePort.findByMemberId(MEMBER_ID)).willReturn(List.of(role));
+        given(getGisuUseCase.getById(GISU_ID)).willReturn(new GisuInfo(GISU_ID, 10L, null, null, true));
+
+        List<ChallengerRoleInfo> result = useCase.listByMemberId(MEMBER_ID);
+
+        assertThat(result).hasSize(1);
+        assertThat(result.getFirst().roleType()).isEqualTo(ChallengerRoleType.SCHOOL_PRESIDENT);
+        assertThat(result.getFirst().gisu()).isEqualTo(10L);
+    }
+
+    @Test
+    @DisplayName("권한 판정 UseCase로 특정 기수의 학교 회장단 여부를 확인한다")
+    void check_school_core_in_gisu() {
+        ChallengerRoleQueryService sut = new ChallengerRoleQueryService(loadChallengerRolePort, getGisuUseCase);
+        CheckChallengerAuthorityUseCase useCase = sut;
+        ChallengerRole role = ChallengerRole.create(
+            10L,
+            ChallengerRoleType.SCHOOL_VICE_PRESIDENT,
+            SCHOOL_ID,
+            null,
+            GISU_ID
+        );
+        given(loadChallengerRolePort.findRolesByMemberIdAndGisuId(MEMBER_ID, GISU_ID)).willReturn(List.of(role));
+
+        boolean result = useCase.isSchoolCoreInGisu(MEMBER_ID, GISU_ID, SCHOOL_ID);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("조회 전용 UseCase로 챌린저별 역할 타입을 일괄 조회한다")
+    void map_role_types_by_challenger_ids() {
+        ChallengerRoleQueryService sut = new ChallengerRoleQueryService(loadChallengerRolePort, getGisuUseCase);
+        ListChallengerRoleUseCase useCase = sut;
+        ChallengerRole role = ChallengerRole.create(
+            10L,
+            ChallengerRoleType.CENTRAL_PRESIDENT,
+            null,
+            null,
+            GISU_ID
+        );
+        given(loadChallengerRolePort.findByChallengerIdIn(Set.of(10L))).willReturn(List.of(role));
+
+        Map<Long, List<ChallengerRoleType>> result = useCase.mapRoleTypesByChallengerIds(Set.of(10L));
+
+        assertThat(result).containsEntry(10L, List.of(ChallengerRoleType.CENTRAL_PRESIDENT));
+    }
+}

UMC-PRODUCT/cygnus-server

src/test/java/com/umc/product/authorization/application/service/query/ChallengerRoleQueryServiceTest.java
@@ -5,6 +5,8 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,17 +15,24 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.umc.product.authorization.application.port.in.query.CheckChallengerAuthorityUseCase;
+import com.umc.product.authorization.application.port.in.query.ListChallengerRoleUseCase;
 import com.umc.product.authorization.application.port.in.query.dto.ChallengerRoleBasicInfo;
+import com.umc.product.authorization.application.port.in.query.dto.ChallengerRoleInfo;
 import com.umc.product.authorization.application.port.out.LoadChallengerRolePort;
 import com.umc.product.authorization.domain.ChallengerRole;
 import com.umc.product.common.domain.enums.ChallengerRoleType;
 import com.umc.product.common.domain.enums.OrganizationType;
 import com.umc.product.organization.application.port.in.query.GetGisuUseCase;
+import com.umc.product.organization.application.port.in.query.dto.gisu.GisuInfo;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("ChallengerRoleQueryService")
 class ChallengerRoleQueryServiceTest {
 
     private static final Long MEMBER_ID = 1L;
+    private static final Long GISU_ID = 9L;
+    private static final Long SCHOOL_ID = 30L;
 
     @Mock
     LoadChallengerRolePort loadChallengerRolePort;
@@ -47,7 +56,7 @@ class ChallengerRoleQueryServiceTest {
         given(loadChallengerRolePort.findByMemberId(MEMBER_ID)).willReturn(List.of(centralRole, schoolRole));
 
         // when
-        List<ChallengerRoleBasicInfo> result = service.findAllBasicByMemberId(MEMBER_ID);
+        List<ChallengerRoleBasicInfo> result = service.listBasicByMemberId(MEMBER_ID);
 
         // then
         assertThat(result).containsExactly(
@@ -57,5 +66,62 @@ class ChallengerRoleQueryServiceTest {
         then(loadChallengerRolePort).should().findByMemberId(MEMBER_ID);
         then(loadChallengerRolePort).shouldHaveNoMoreInteractions();
         then(getGisuUseCase).shouldHaveNoInteractions();
+    }
+
+    @Test
+    @DisplayName("조회 전용 UseCase로 멤버의 역할 목록을 조회한다")
+    void list_by_member_id() {
+        ListChallengerRoleUseCase useCase = service;
+        ChallengerRole role = ChallengerRole.create(
+            10L,
+            ChallengerRoleType.SCHOOL_PRESIDENT,
+            SCHOOL_ID,
+            null,
+            GISU_ID
+        );
+        given(loadChallengerRolePort.findByMemberId(MEMBER_ID)).willReturn(List.of(role));
+        given(getGisuUseCase.getById(GISU_ID)).willReturn(new GisuInfo(GISU_ID, 10L, null, null, true));
+
+        List<ChallengerRoleInfo> result = useCase.listByMemberId(MEMBER_ID);
+
+        assertThat(result).hasSize(1);
+        assertThat(result.getFirst().roleType()).isEqualTo(ChallengerRoleType.SCHOOL_PRESIDENT);
+        assertThat(result.getFirst().gisu()).isEqualTo(10L);
+    }
+
+    @Test
+    @DisplayName("권한 판정 UseCase로 특정 기수의 학교 회장단 여부를 확인한다")
+    void check_school_core_in_gisu() {
+        CheckChallengerAuthorityUseCase useCase = service;
+        ChallengerRole role = ChallengerRole.create(
+            10L,
+            ChallengerRoleType.SCHOOL_VICE_PRESIDENT,
+            SCHOOL_ID,
+            null,
+            GISU_ID
+        );
+        given(loadChallengerRolePort.findRolesByMemberIdAndGisuId(MEMBER_ID, GISU_ID)).willReturn(List.of(role));
+
+        boolean result = useCase.isSchoolCoreInGisu(MEMBER_ID, GISU_ID, SCHOOL_ID);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("조회 전용 UseCase로 챌린저별 역할 타입을 일괄 조회한다")
+    void map_role_types_by_challenger_ids() {
+        ListChallengerRoleUseCase useCase = service;
+        ChallengerRole role = ChallengerRole.create(
+            10L,
+            ChallengerRoleType.CENTRAL_PRESIDENT,
+            null,
+            null,
+            GISU_ID
+        );
+        given(loadChallengerRolePort.findByChallengerIdIn(Set.of(10L))).willReturn(List.of(role));
+
+        Map<Long, List<ChallengerRoleType>> result = useCase.mapRoleTypesByChallengerIds(Set.of(10L));
+
+        assertThat(result).containsEntry(10L, List.of(ChallengerRoleType.CENTRAL_PRESIDENT));
     }
 }