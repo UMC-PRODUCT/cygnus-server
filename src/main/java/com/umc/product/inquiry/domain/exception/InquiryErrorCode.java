8030adacc532ef6d8512e0dfaaa05263bbc0e0b8
sopp1313 <dldpdms0131@gmail.com>
2026-07-17 19:26:18 +0900

    fix: 유령 파일 제거

--- src/main/java/com/umc/product/inquiry/domain/exception/InquiryErrorCode.java
+++ src/main/java/com/umc/product/inquiry/domain/exception/InquiryErrorCode.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 public enum InquiryErrorCode implements BaseCode {
 
     INQUIRY_NOT_FOUND(HttpStatus.NOT_FOUND, "INQUIRY-0001", "문의사항을 찾을 수 없습니다."),
-    INQUIRY_MESSAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "INQUIRY-0002", "문의사항 메시지를 찾을 수 없습니다."),
     INQUIRY_ALREADY_CLOSED(HttpStatus.BAD_REQUEST, "INQUIRY-0003", "이미 종료된 문의사항입니다."),
     NO_INQUIRY_PERMISSION(HttpStatus.FORBIDDEN, "INQUIRY-0004", "해당 문의사항에 접근할 권한이 없습니다."),
     INQUIRY_INVALID_STATUS_FOR_PROGRESS(HttpStatus.BAD_REQUEST, "INQUIRY-0005",
